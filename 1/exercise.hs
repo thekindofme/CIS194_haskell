3da137cdc3c8d7c75bfa8073cee21ea40d6600e1
Yasith Fernando <thekindofme@gmail.com>
2014-12-14 18:52:32 +1000

    Complete the exercise

--- 1/exercise.hs
+++ 1/exercise.hs
@@ -7,11 +7,27 @@ dropLastDigit x = x `div` 10
 
 toDigits :: Integer -> [Integer]
 toDigits x
-| x < 1 = []
-| otherwise = toDigits(dropLastDigit(x)) ++ [lastDigit(x)]
+  | x < 1 = []
+  | otherwise = toDigits(dropLastDigit(x)) ++ [lastDigit(x)]
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (p:ps)
-| (length(ps) `mod` 2 == 0) = p : doubleEveryOther ps
-| otherwise = (p*2) : doubleEveryOther ps
+  | (length(ps) `mod` 2 == 0) = p : doubleEveryOther ps
+  | otherwise = (p*2) : doubleEveryOther ps
+
+--
+-- Example: sumDigits [16,7,12,5] = 1 + 6 + 7 + 1 + 2 + 5 = 22
+--
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (p: ps)
+  | p < 10 = (p + (sumDigits ps))
+  | otherwise = (sum (toDigits p)) + (sumDigits ps)
+
+--
+-- Example: validate 4012888888881881 = True
+-- Example: validate 4012888888881882 = False
+--
+validate :: Integer -> Bool
+validate card_number = ((sumDigits (doubleEveryOther (toDigits card_number))) `mod` 10) == 0
